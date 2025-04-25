98345fa0ff16eeb1aa0b42841605adfbc1561558
Nicolas <nicolas.gracioli@gmail.com>
2025-04-25 19:42:41 -0300

    refactor: improve code structure in index.js

--- backend/index.js
+++ backend/index.js
@@ -7,24 +7,27 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-async function connectDB() {
+async function startServer() {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connection sucessfull");
+
+        // Rotas
+        const urlRoutes = require("./routes/urlRoutes");
+        app.use("/", urlRoutes);
+
+        app.get("/", (req, res) => {
+            res.send("Working API!");
+        });
+
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+        });
+
     } catch (err) {
-        console.error("Error:", err);
+        console.error("Database connection error:", err);
+        process.exit(1);
     }
 }
 
-connectDB();
-
-const urlRoutes = require("./routes/urlRoutes");
-app.use("/", urlRoutes);
-
-app.get("/", (req, res) => {
-    res.send("working!");
-});
-
-app.listen(port, () => {
-    console.log(`Running in ${port}`);
-});
+startServer();
