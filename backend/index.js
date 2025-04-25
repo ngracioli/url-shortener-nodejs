d3ff6e58ca493be6a296f082806c7a3c965b6a2a
Nicolas <nicolas.gracioli@gmail.com>
2025-04-25 18:39:29 -0300

    feat: add mongoose configuration to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connection sucessfull");
+    } catch (err) {
+        console.error("Error:", err);
+    }
+}
+
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("working!");
